4d3e90bc27c6c827d985bd7f3abfca27ce4447f8
Axel Resendiz <armzmcav@gmail.com>
2025-10-09 16:46:22 -0600

    GetImages

--- frontend-ecommerce/components/index/featured-products.tsx
+++ frontend-ecommerce/components/index/featured-products.tsx
@@ -93,7 +93,7 @@ const FeaturedProducts = () => {
                           <div className="relative overflow-hidden bg-white h-48 flex items-center justify-center">
                             {imageUrl ? (
                               <img
-                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${imageUrl}`}
+                                src={`${imageUrl}`}
                                 alt={productName}
                                 className="max-w-full max-h-full object-contain group-hover:scale-105 transition-transform duration-300"
                                 loading="lazy"
